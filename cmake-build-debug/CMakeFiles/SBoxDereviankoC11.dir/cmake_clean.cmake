c80b2a633450a4d8a678edb83d939d5e37a3a9a3
olenaderevianko <dereviankoolenav@gmail.com>
2021-09-08 15:45:51 +0300

    C11

---
+++ cmake-build-debug/CMakeFiles/SBoxDereviankoC11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SBoxDereviankoC11.dir/main.c.obj"
+  "SBoxDereviankoC11.exe"
+  "SBoxDereviankoC11.exe.manifest"
+  "SBoxDereviankoC11.pdb"
+  "libSBoxDereviankoC11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SBoxDereviankoC11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
